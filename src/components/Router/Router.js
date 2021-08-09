cc5dd632ae49fad2d968794e6a6047f093b24834
Alex <nerv11@mail.ru>
2021-08-09 21:50:17 +0300

    Add HW8

---
+++ src/components/Router/Router.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+import '../App/App.css'
+import Chat from '../Chat/Chat'
+import Home from '../Home'
+import Chats from '../Chats/Chats'
+import Profile from '../Profile/Profile'
+import HomeContainer from '../Home/HomeContainer'
+import News from '../News'
+import Login from '../Login'
+import { useSelector } from 'react-redux'
+
+const PrivateRoute = (props) => {
+    const isAuthed = useSelector((state) => state.profile.isAuthed)
+
+    return isAuthed ? <Route {...props} /> : <Redirect to="/login" />
+}
+
+export default function Router() {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                exact
+                render={() => (
+                    <React.Fragment>
+                        <p>Container</p>
+                        <HomeContainer />
+
+                        <p>Component</p>
+                        <Home
+                            age={12}
+                            name={'Alice'}
+                            onChangeProfileName={(name) => console.log(name)}
+                        />
+                    </React.Fragment>
+                )}
+            />
+
+            <PrivateRoute exact path="/chats" component={Chats} />
+
+            <PrivateRoute path="/chats/:chatId" component={Chat} />
+
+            <PrivateRoute path="/profile">
+                <Profile />
+            </PrivateRoute>
+
+            <Route path="/login" component={Login} />
+
+            <Route path="/news" component={News} />
+
+            <Route>
+                <p>404: not found</p>
+            </Route>
+        </Switch>
+    )
+}
